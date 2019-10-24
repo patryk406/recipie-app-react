aa8c87dee6f016bcdb0495bb5cf6eac2c97b8562
patryk406 <Merph406@gmail.com>
2019-10-24 10:55:40 +0200

    build

---
+++ build/precache-manifest.fd51ba3334c71ecf466a5c14e32561d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2de8355b267a87c30a9ae6d5c5060cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3df4075cd95126b9a23a",
+    "url": "/static/css/main.25cb708d.chunk.css"
+  },
+  {
+    "revision": "f009ab747a2e3223e527",
+    "url": "/static/js/2.ab6e2efe.chunk.js"
+  },
+  {
+    "revision": "3df4075cd95126b9a23a",
+    "url": "/static/js/main.b86e8eb1.chunk.js"
+  },
+  {
+    "revision": "8132094dbd6229449c58",
+    "url": "/static/js/runtime-main.cf502de6.js"
+  },
+  {
+    "revision": "374344147cff8606f835d752944d8570",
+    "url": "/static/media/light-grey-terrazzo.37434414.png"
+  }
+]);
