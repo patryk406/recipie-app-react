24f3ffbabbda7581f6f14fcf0d0d117defbdd8d7
patryk406 <Merph406@gmail.com>
2019-10-27 12:38:23 +0100

    responsive for mobile

--- src/Recipe.js
+++ src/Recipe.js
@@ -6,7 +6,7 @@ import style from './recipe.module.css';
 const Recipe = ({ title, image, ingredients }) => {
   return (
     <div className={style.recipe}>
-      <h1>{title}</h1>
+      <h1 maxLength='16'>{title}</h1>
 
       <div className='content'>
         <img className={style.image} src={image} alt='' />
