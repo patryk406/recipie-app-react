36e432beb0f7c46282fea9d3592249e8e122e656
patryk406 <Merph406@gmail.com>
2019-11-18 20:10:51 +0100

    add bootstrap, change style, reconstruction of app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import Recipe from './Recipe';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Recipe from "./Recipe";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
-  const APP_ID = 'd1f8428e';
-  const APP_KEY = 'ca7e25fde70993eb934d61e684879b13';
+  const APP_ID = "d1f8428e";
+  const APP_KEY = "ca7e25fde70993eb934d61e684879b13";
 
   const [recipes, setRecipes] = useState([]);
-  const [search, setSearch] = useState('');
-  const [query, setQuery] = useState('chicken');
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
 
   useEffect(() => {
     getRecipes();
@@ -21,6 +22,7 @@ const App = () => {
     );
     const data = await response.json();
     setRecipes(data.hits);
+    console.log(data);
   };
 
   const updateSearch = e => {
@@ -30,35 +32,61 @@ const App = () => {
   const getSearch = e => {
     e.preventDefault();
     setQuery(search);
-    setSearch('');
+    setSearch("");
   };
 
   return (
-    <div className='App'>
-      <form onSubmit={getSearch} className='search-form'>
+    <div className="App">
+      <form onSubmit={getSearch} className="search-form">
         <input
-          className='search-bar'
-          type='text'
+          className="search-bar"
+          type="text"
           value={search}
           onChange={updateSearch}
-          placeholder='Write what dish You wanna eat'
+          placeholder="Write what dish You wanna eat"
         />
-        <button className='search-button' type='submit'>
+        <button className="btn btn-outline-dark" type="submit">
           Search
         </button>
       </form>
 
-      <div className='recipes'>
+      <div className="recipes">
         {recipes.map(recipe => (
           //Przypisanie potrzebnych danych z jsona z data.
           <Recipe
             key={recipe.recipe.label}
             title={recipe.recipe.label}
             image={recipe.recipe.image}
-            ingredients={recipe.recipe.ingredients}
+            calories={recipe.recipe.calories}
+            time={recipe.recipe.totalTime}
+            length={recipe.recipe.ingredients.length}
+            source={recipe.recipe.source}
+            link={recipe.recipe.url}
           />
         ))}
       </div>
+      <footer className="text-center">
+        <h6>
+          If You want more detailed recipies, please write ingredients witch You
+          wanna use to cook.
+        </h6>
+        <h6>
+          For more results go to{" "}
+          <a href="https://edamam.com/" target="_blank">
+            EDAMAM
+          </a>{" "}
+          or any of sites from your search.
+        </h6>
+        <p>
+          App powered by
+          <a href="https://pl.reactjs.org/" target="_blank">
+            <img
+              src="https://img.icons8.com/nolan/64/000000/react-native.png"
+              alt="#"
+            />
+          </a>
+        </p>
+      </footer>
     </div>
   );
 };
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,23 +1,52 @@
-import React from 'react';
-import style from './recipe.module.css';
+import React from "react";
+import "./recipie.css";
 
 //wyslanie danych, ktore zostana nadpisane poprzez jsona z data na stronie.
 
-const Recipe = ({ title, image, ingredients }) => {
+const Recipe = ({
+  title,
+  image,
+  key,
+  calories,
+  length,
+  time,
+  link,
+  source
+}) => {
   return (
-    <div className={style.recipe}>
-      <h1 maxLength='16'>{title}</h1>
+    <div className="card-deck">
+      <div className="card">
+        <h3 className="card-title text-center">{title}</h3>
+        <h6 className="card-footer">
+          <p className="upper">
+            Ingredients:<span className="length"> {length}</span>
+          </p>
+          <p className="upper">
+            Cook time: <span className="time">{time} min</span>
+          </p>
+        </h6>
 
-      <div className='content'>
-        <img className={style.image} src={image} alt='' />
-        <ol>
-          {ingredients.map(ingredients => (
-            <li>{ingredients.text}</li>
-          ))}
-        </ol>
+        <img className="card-img" src={image} alt="food" />
+        <div className="card-text">
+          <h5 className="text-center py-2">{cal()} Calories</h5>
+        </div>
+        <div className="card-footer">
+          <p>
+            Have whole recipie in{" "}
+            <a href={link} target="_blank">
+              {source}
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
+
+  function cal() {
+    if (calories) {
+      return Math.round(calories / 10);
+    }
+  }
 };
 
 export default Recipe;
